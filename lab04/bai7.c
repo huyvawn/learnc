974e24296cdf598d71157f986712fb6225270bbe
huyvawn <63355258+huyvawn@users.noreply.github.com>
2024-04-15 11:57:52 +0700

    update lab04 bai7

---
+++ lab04/bai7.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+
+//int bai7(){
+int main() {
+    int num1, num2;
+    printf("Nhap 2 so bat ki:\n");
+    scanf("%d %d", &num1, &num2);
+
+    if (num1 % num2 == 0)
+        printf("%d la uoc cua %d", num2, num1);
+    else if (num2 % num1 == 0)
+        printf("%d la uoc cua %d", num1, num2);
+    return 0;
+}

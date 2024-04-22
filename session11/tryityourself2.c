5a316a96a79f45bcefe5b487009b59e44a27ae70
huyvawn <63355258+huyvawn@users.noreply.github.com>
2024-04-22 12:04:52 +0700

    update session11

--- session11/tryityourself2.c
+++ session11/tryityourself2.c
@@ -1,22 +1,26 @@
-#include "stdio.h"
-#include "string.h"
-#include "conio.h"
-int try11_2(){
+//#include "stdio.h"
+//#include "string.h"
+//#include "conio.h"
+//int try11_2(){
 //    int main(){
-    char text[100];
-    int count=0;
-    printf("Enter a line of text (max 100 characters): \n");
-    scanf("%[^\n]s", text);
-    for(int i = 0; i<99; i++){
-        switch (text[i]) {
-            case'a':
-            case'e':
-            case'i':
-            case'o':
-            case'u':
-                count++;
-        }
-    }
-    printf("Number of vowels is %d", count);
-    return 0;
-}
+//    char text[100];
+//    int count=0;
+//    printf("Enter a line of text (max 100 characters): \n");
+//    scanf("%[^\n]s", text);
+//    for(int i = 0; i<99; i++){
+//        switch (text[i]) {
+//            case'a':
+//            case'e':
+//            case'i':
+//            case'o':
+//            case'u':
+//                count++;
+//                break;
+//            case'\n':
+//                break;
+//        }
+//        printf("%c\t", text[i]);
+//    }
+//    printf("\nNumber of vowels is %d", count);
+//    return 0;
+//}
